bc4bcf8fcb24f1a8b71f195edfa6e4fb2d6c8994
LEESANGNAM <fgh0116@naver.com>
2021-04-09 21:23:41 +0900

    6주차

---
+++ Ex5/app/src/main/java/kr/ac/yeonsung/fgh0116/ex5/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.yeonsung.fgh0116.ex5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex5_1_2);
+    }
+}

9d5352fd9587b4bdba8a550ba8c13b64cbeb80a3
LEESANGNAM <fgh0116@naver.com>
2021-05-21 11:10:48 +0900

    12주

---
+++ ex6/app/src/main/java/kr/ac/yeonsung/fgh0116/ex6/MainActivity2.java
@@ -0,0 +1,19 @@
+package kr.ac.yeonsung.fgh0116.ex6;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex6_12);
+
+
+    }
+}
